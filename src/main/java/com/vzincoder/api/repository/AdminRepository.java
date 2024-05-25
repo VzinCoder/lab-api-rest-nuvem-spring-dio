e77df2fcd7a10e21f618ad67bc2ecdef00f16094
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-24 22:54:53 -0300

    add new method

--- src/main/java/com/vzincoder/api/repository/AdminRepository.java
+++ src/main/java/com/vzincoder/api/repository/AdminRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vzincoder.api.domain.Admin;
+import java.util.Optional;
+
 
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
+
+    public Optional<Admin> findByEmail(String email);
 } 
