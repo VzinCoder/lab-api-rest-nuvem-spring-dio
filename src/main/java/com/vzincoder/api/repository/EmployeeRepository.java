214a76cf40505081eccaa9c7c43b0d5214fb1df3
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-27 12:12:12 -0300

    update repository employee

--- src/main/java/com/vzincoder/api/repository/EmployeeRepository.java
+++ src/main/java/com/vzincoder/api/repository/EmployeeRepository.java
@@ -1,11 +1,18 @@
 package com.vzincoder.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.vzincoder.api.domain.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+    public Optional<Employee> findByEmail(String email);
+
+    public Optional<Employee> findByCpf(String cpf);
+
+    public boolean existsByEmail(String email);
+
+    public boolean existsByCpf(String cpf);
 }
